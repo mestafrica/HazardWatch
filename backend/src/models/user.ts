4a9552a033a580da63b233772388445105c8dc0f
PriscaKing <priscyking3@gmail.com>
2024-08-01 22:36:58 +0000

    "created a recovery pass"

--- backend/src/models/user.ts
+++ backend/src/models/user.ts
@@ -1,5 +1,6 @@
 import mongoose, {Schema, Types } from "mongoose";
 import IUser from "../interfaces/user";
+// import { hash } from "bcryptjs";
 
 const UserSchema: Schema = new Schema({
     firstName: {type: String, required:true},
@@ -13,5 +14,20 @@ const UserSchema: Schema = new Schema({
 {
     timestamps:true
 })
+
+// //hash password before saving with mongoose pre save hook
+// UserSchema.pre('save',async function(next){
+//     //if password is not different move to next middleware in the chain
+//     if(!this.isModified("password")) {
+//         return next()
+//     }
+
+//     //otherwise save hash password
+//     this.password= await hash(this.password, 10)
+// })
+
+//use existing model or create a newone
+// const User=models.User || model("User", UserSchema)
+
 
 export default mongoose.model<IUser>('User', UserSchema)
