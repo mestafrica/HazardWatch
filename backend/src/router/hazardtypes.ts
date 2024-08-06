9fcd4a772a176ea84f631152529fcc167e1a3512
Dan Sidsaya <dansidsaya@gmail.com>
2024-08-06 13:43:08 +0000

    Auth to Routes

--- backend/src/router/hazardtypes.ts
+++ backend/src/router/hazardtypes.ts
@@ -1,12 +1,13 @@
 import express from 'express';
 import controller from '../controllers/hazardtypes';
+import extractJWT from 'middlewares/extractJWT';
 
 const router = express.Router();
 
-router.post('/create', controller.createHazardType);
-router.get('/all', controller.getAllHazardTypes);
-router.get('/:id', controller.getHazardTypeById);
-router.patch('/update/:id', controller.updateHazardType);
-router.delete('/delete/:id', controller.deleteHazardType);
+router.post('/create',extractJWT, controller.createHazardType);
+router.get('/all',extractJWT, controller.getAllHazardTypes);
+router.get('/:id',extractJWT, controller.getHazardTypeById);
+router.patch('/update/:id',extractJWT, controller.updateHazardType);
+router.delete('/delete/:id',extractJWT, controller.deleteHazardType);
 
 export = router;
