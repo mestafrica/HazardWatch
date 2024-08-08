42f4778f1d97795d1d8adbbe2d96f61351c5302c
EunicePobee <arabaawilba@gmail.com>
2024-08-08 07:47:58 +0000

    More changes for api deployment

---
+++ backend/dist/config/config.js
@@ -0,0 +1,36 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
+// Load environment variables from .env file
+const MONGO_URL = process.env.MONGO_URL || 'mongodb://localhost:1337/defaultDB';
+const MONGO_OPTIONS = {
+    socketTimeoutMS: Number(process.env.MONGO_OPTIONS_SOCKET_TIMEOUT_MS) || 30000,
+    autoIndex: process.env.MONGO_OPTIONS_AUTO_INDEX === 'true',
+    retryWrites: process.env.MONGO_OPTIONS_RETRY_WRITES === 'true',
+};
+const SERVER_HOSTNAME = process.env.SERVER_HOSTNAME || 'localhost';
+const SERVER_PORT = Number(process.env.SERVER_PORT) || 1337;
+const SERVER_TOKEN_EXPIRETIME = Number(process.env.SERVER_TOKEN_EXPIRETIME) || 3600;
+const SERVER_TOKEN_ISSUER = process.env.SERVER_TOKEN_ISSUER || 'coolIssuer';
+const SERVER_TOKEN_SECRET = process.env.SERVER_TOKEN_SECRET || 'defaultSecret';
+const config = {
+    mongo: {
+        url: MONGO_URL,
+        options: MONGO_OPTIONS,
+    },
+    server: {
+        hostname: SERVER_HOSTNAME,
+        port: SERVER_PORT,
+        token: {
+            expireTime: SERVER_TOKEN_EXPIRETIME,
+            issuer: SERVER_TOKEN_ISSUER,
+            secret: SERVER_TOKEN_SECRET,
+        },
+    },
+};
+exports.default = config;
+//# sourceMappingURL=config.js.map
