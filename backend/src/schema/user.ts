2ba41f85d085935ab12b9ff51fd2eec6c62a5f82
EunicePobee <arabaawilba@gmail.com>
2024-08-15 20:31:50 +0000

    merge for deployment

--- backend/src/schema/user.ts
+++ backend/src/schema/user.ts
@@ -1,15 +1,16 @@
 import Joi from "joi";
 
 export const registerValidator = Joi.object({
-    name: Joi.string().required(),
-    username: Joi.string().required(),
+    firstName: Joi.string().required(),
+    lastName: Joi.string().required(),
+    userName: Joi.string().required(),
     email: Joi.string().email().required(),
     password: Joi.string().required(),
     confirmPassword: Joi.string().valid(Joi.ref('password')).required(),
 });
 
 export const loginValidator = Joi.object({
-    username: Joi.string(),
+    userName: Joi.string(),
     email: Joi.string().email(),
     password: Joi.string().required(),
 });
@@ -25,8 +26,9 @@ export const resetPasswordValidator = Joi.object({
 });
 
 export const createUserValidator = Joi.object({
-    name: Joi.string().required(),
-    username: Joi.string().required(),
+    firstName: Joi.string().required(),
+    lastName: Joi.string().required(),
+    userName: Joi.string().required(),
     email: Joi.string().email().required(),
     password: Joi.string().required(),
     role: Joi.string().required().valid('admin', 'manager'),
